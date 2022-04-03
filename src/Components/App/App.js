7fb00cc325573c13ff251349ddf3d2d183ea970e
Silvers Rayleigh <srayleighdk@gmail.com>
2022-04-03 09:40:10 -0400

    second commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className='bg-violet-800 px-0 py-4 text-2xl font-sans font-bold text-center text-white'>Ja<span className='text-purple-500'>mmm</span>ing</h1>
+      <div className='App h-full w-full bg-background-app'>
+        {/* Add a SearchBar component */}
+        <div className='App-playlist'>
+          {/* Add a SearchResults component */}
+          {/* Add a Playlistcomponent */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'background-app': "url('/home/srayleighdk/Documents/project-jimming/src/Components/App/background.jpg')",
+      }
+    },
+  },
+  plugins: [],
+}
